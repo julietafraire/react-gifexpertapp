c081caa1c2737f617430a5efc359a69f3514635a
Julieta Fraire <julietafederica@hotmail.com>
2020-07-05 20:18:18 -0300

    docs agregado

---
+++ docs/precache-manifest.54fabaa8246de521adcff413dbb4e0b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3764e323db18f9253d58e3b9dc55108d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ccee5d9d9fff4a9e5ec",
+    "url": "/static/css/main.6c596a41.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ccee5d9d9fff4a9e5ec",
+    "url": "/static/js/main.7d0e2a81.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
